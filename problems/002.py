5066f4d514b05c7de7e00b1940ebf69a199284ed
chris <cjsimpson@gmail.com>
2012-11-03 14:35:11 -0400

    Problem 2

---
+++ problems/002.py
@@ -0,0 +1,17 @@
+'''
+Project Eurler Problem 2
+http://projecteuler.net/problem=2
+'''
+
+LIMIT = 4000000
+SEEDS = (1,2)
+
+def fib(limit):
+    fib_sequence = list(SEEDS)
+    while fib_sequence[-1] + fib_sequence[-2] < limit:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+print sum(x for x in fib(LIMIT) if x%2 ==0)
+    
+        
