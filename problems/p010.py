4c84971367f1b521cacd64181d5d8b033bc9ebbe
chris <cjsimpson@gmail.com>
2012-11-03 23:46:21 -0400

    Optimizing Problem 10. Still room for improvement.

--- problems/p010.py
+++ problems/p010.py
@@ -2,24 +2,33 @@
 Project Eurler Problem 10
 http://projecteuler.net/problem=10
 '''
-#TODO: This runs too slow
+#TODO: This still runs too slow
 
 def is_prime(number):
     if number in (0,1):
         return False
-    for i in range(2, int(number**0.5) + 1):
+    for i in xrange(2, int(number**0.5) + 1):
         if number % i == 0:
             return False
     return True
 
 def primes_in_range(low, high):
-    for x in xrange(low, high):
-        if is_prime(x):
-            yield x
- 
+    
+    def remove_multiples(source, n):
+        to_discard = set()
+        for x in source:
+            if x!=n and x%n ==0:
+                to_discard.add(x)
+        source -= to_discard
+        return source
+    
+    potential_primes = set(range(low, high+1))
+    potential_primes.discard(1)
+    
+    for k in xrange(2, int(high**0.5)+1 ):
+        if is_prime(k):
+            remove_multiples(potential_primes, k)
+    return potential_primes
+
 if __name__ == '__main__':
-    sum_of_primes = 0
-    for x in primes_in_range(1,2000000):
-        sum_of_primes += x
-    print
-    print sum_of_primes
+    print sum(primes_in_range(1,2000000))
