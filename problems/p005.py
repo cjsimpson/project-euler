30b6a938da5b21dd5856ed3aa6a0ace6d42c2de4
chris <cjsimpson@gmail.com>
2012-11-03 20:15:41 -0400

    Refactor p005.py to use prime number factorization method instead of brute force.

--- problems/p005.py
+++ problems/p005.py
@@ -3,43 +3,41 @@ Project Eurler Problem 4
 http://projecteuler.net/problem=4
 '''
 
-#TODO: This is too slow. Find a way to optimize
+from p003 import find_prime_factors, find_factors, is_prime
 
-from p003 import find_prime_factors
-
-def is_divisable_by_all_nums(list_of_numbers, dividend):
-    for num in list_of_numbers:
-        if dividend % num != 0:
-            return False
-    return True
-
-def find_highest_factors(list_of_numbers):
-    #TODO: This isn't very clean
-    number_set = set(list_of_numbers)
-    if 1 in number_set:
-        number_set.remove(1)
-    for x in list_of_numbers[::-1]:
-        for y in list_of_numbers:
-            if (x != 1 and y != 1 and x!=y) and (x % y == 0):
-                try:
-                    number_set.remove(y)
-                except KeyError:
-                    pass
-    return number_set
-
-def find_smallest_dividend(list_of_numbers):
-    list_of_numbers = list(find_highest_factors(list_of_numbers))
-    list_of_numbers.sort(reverse=True)
-    print list_of_numbers
-    lower_limit = max(list_of_numbers) * min(list_of_numbers)
-    upper_limit = reduce(lambda x,y: x*y, list_of_numbers)
+def find_prime_factorization(number):
+    all_factors = find_factors(number)
+    prime_factorization = []
+    if len(all_factors) == 1:
+        prime_factorization.extend([x for x in all_factors[0]])
+    else:
+        all_factors.remove((1,number))
+        factor_pair = all_factors.pop()
     
-    print 'MIN %d' %  lower_limit
-    print 'MAX %d' %  upper_limit
+        for f in factor_pair:
+            if is_prime(f):
+                prime_factorization.append(f)
+            elif f not in (1, number):
+                map(lambda x: prime_factorization.append(x), find_prime_factors(f))
+    return prime_factorization
+        
+def find_lowest_common_multiple(list_of_numbers):
+    if 1 in list_of_numbers:
+        list_of_numbers.remove(1)
+    prime_factors = {x:find_prime_factorization(x) for x in list_of_numbers}
+    primes = prime_factors.values()
+    prime_count = {}
+    for x in primes:
+        for y in x:
+            count = x.count(y)
+            if y in prime_count:
+                if count > prime_count[y]:
+                    prime_count[y] = count
+            else:
+                prime_count[y] = count
     
-    for num in xrange(max(list_of_numbers), upper_limit):
-        if is_divisable_by_all_nums(list_of_numbers, num):
-            return num
-            
+    result = reduce(lambda x,y: x*y, [k**v for k,v in prime_count.iteritems()])
+    return result
 
-print find_smallest_dividend(range(1,11))
+if __name__ == '__main__':
+    print find_lowest_common_multiple(range(1,21))
