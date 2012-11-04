2561bb74c1b1c63ee373307b8fcfe58c1caf02f2
chris <cjsimpson@gmail.com>
2012-11-03 20:32:33 -0400

    Problem 7

---
+++ problems/p007.py
@@ -0,0 +1,18 @@
+'''
+Project Eurler Problem 7
+http://projecteuler.net/problem=7
+'''
+
+from p003 import is_prime
+
+def find_nth_prime(n):
+    prime_list = set()
+    x = 1
+    while len(prime_list) < n:
+        if is_prime(x):
+            prime_list.add(x)
+        x += 1
+    return max(prime_list)
+
+if __name__ == '__main__':
+    print find_nth_prime(10001)
