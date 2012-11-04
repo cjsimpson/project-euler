d55c5ab9debb18a0ef78935a518230a366826027
chris <cjsimpson@gmail.com>
2012-11-03 20:24:04 -0400

    Problem 6

---
+++ problems/p006.py
@@ -0,0 +1,15 @@
+'''
+Project Eurler Problem 4
+http://projecteuler.net/problem=6
+'''
+
+def find_sum_of_squares(numbers):
+    return sum(x**2 for x in numbers)
+
+def find_square_of_sums(numbers):
+    return sum(numbers)**2
+
+if __name__ == '__main__':
+    r = range(1,101)
+    result = find_square_of_sums(r) - find_sum_of_squares(r)
+    print result
